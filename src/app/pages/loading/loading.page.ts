23fb560ea8de9fc253c409e19d6b604aa49b0fde
PabloGM1204 <pgarmun203@g.educaand.es>
2024-02-25 19:43:31 +0100

    Pagina de loading horizontal

--- src/app/pages/loading/loading.page.ts
+++ src/app/pages/loading/loading.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ApiService } from 'src/app/core/services/api/api.service';
 import { AuthService } from 'src/app/core/services/api/strapi/auth.service';
+import { ScreenOrientation } from '@awesome-cordova-plugins/screen-orientation/ngx';
 
 @Component({
   selector: 'app-loading',
@@ -12,10 +13,12 @@ export class LoadingPage implements OnInit {
 
   constructor(
     private apiSvc: AuthService,
-    private router: Router
+    private router: Router,
+    private screen: ScreenOrientation
   ) { }
 
   ngOnInit() {
+    this.screen.lock(this.screen.ORIENTATIONS.LANDSCAPE)
     this.apiSvc.isLogged$.subscribe();
     setTimeout(() => {
       this.router.navigate(['/home'])
